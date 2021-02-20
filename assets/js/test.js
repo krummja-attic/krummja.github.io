3f90c30cc752cb5e2efcfdff1b7588b5a114fa89
Jonathan Crum <crumja@uga.edu>
2021-02-19 22:57:45 -0500

    .

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,6 +1,10 @@
 const scale = 22;
 let root = d3.select("div#js-tests");
 
+root.append("svg")
+    .attr('width', 25*scale)
+    .attr('height', 10*scale);
+
 for (let x = 0; x < 25; x++) {
     for (let y = 0; y < 10; y++) {
         root.append('rect')
