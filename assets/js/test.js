a7b5fc094004ffaa6491e7b96eb4ce5756ac0b2e
Jonathan Crum <crumja@uga.edu>
2021-02-19 23:17:25 -0500

    .

--- assets/js/test.js
+++ assets/js/test.js
@@ -17,3 +17,16 @@ for (let x = 0; x < 25; x++) {
            .attr('stroke', "gray");
     }
 }
+
+let A = {x: 2, y: 2}, B = {x: 20, y: 8};
+let N = Math.max(Math.abs(A.x - B.x), Math.abs(A.y - B.y));
+for (let i = 0; i <= N; i++) {
+    let t = i / N;
+    let x = Math.round(A.x + (B.x - A.x) * t);
+    let y = Math.round(A.y + (B.y - A.y) * t);
+    svg.append('rect')
+       .attr('transform', `translate(${x*scale}, ${y*scale})`)
+       .attr('width', scale-1)
+       .attr('height', scale-1)
+       .attr('fill', "hsl(0,40%,70%)");
+}
