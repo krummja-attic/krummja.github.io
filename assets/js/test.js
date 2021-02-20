7f52d15b8cce24f6e94aff7cdb5cf18c9bed5ae0
Jonathan Crum <crumja@uga.edu>
2021-02-19 23:13:06 -0500

    .

--- assets/js/test.js
+++ assets/js/test.js
@@ -5,15 +5,15 @@ root.append("svg")
     .attr('width', 25*scale)
     .attr('height', 10*scale);
 
-let svg = d3.select("svg")
+let svg = d3.select("div#js-tests svg")
 
 for (let x = 0; x < 25; x++) {
     for (let y = 0; y < 10; y++) {
         svg.append('rect')
-            .attr('transform', `translate(${x*scale}, ${y*scale})`)
-            .attr('width', scale)
-            .attr('height', scale)
-            .attr('fill', "white")
-            .attr('stroke', "gray");
+           .attr('transform', `translate(${x*scale}, ${y*scale})`)
+           .attr('width', scale)
+           .attr('height', scale)
+           .attr('fill', "white")
+           .attr('stroke', "gray");
     }
 }
