315a2274f80cc5bd04943b1649a92d1bc3104079
Jonathan Crum <crumja@uga.edu>
2021-02-27 21:03:39 -0500

    .

--- _nuxt/static/1614477447/blog/article_3/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614477447",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Farticle_3",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4c86758b"}}}}(null));
