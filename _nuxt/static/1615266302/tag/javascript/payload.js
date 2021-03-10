3868583f26f1a3a2f9a1fef897dcf9ed5112c77a
Jonathan Crum <crumja@uga.edu>
2021-03-09 21:51:45 -0500

    multiple updates

--- _nuxt/static/1615266302/tag/javascript/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/tag/javascript", (function(a){return {data:[{articles:[],notes:[],tag:{slug:a,name:a,toc:[],body:{type:"root",children:[]},dir:"\u002Ftags",path:"\u002Ftags\u002Fjavascript",extension:".md",createdAt:"2021-03-04T02:02:25.477Z",updatedAt:"2021-03-04T02:02:29.113Z"}}],fetch:{},mutations:void 0}}("javascript")));
