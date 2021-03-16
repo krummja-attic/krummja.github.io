718ee43ef3c99963a9daba39afe5069e0862a8f0
Jonathan Crum <crumja@uga.edu>
2021-03-15 22:08:24 -0400

    General update

--- _nuxt/static/1615344676/tag/research/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615344676",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftag\u002Fresearch",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a600d948"}}}}(null));
---
+++ _nuxt/static/1615860469/experimental/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/experimental", {data:[{}],fetch:{},mutations:void 0});
