6514f8348f35fbe40fbfe5d9a55a899cd8f33a45
Jonathan Crum <crumja@uga.edu>
2021-03-31 23:45:51 -0400

    Tag system working

---
+++ _nuxt/static/1617248730/garden/tags/Linguistics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617248730",layout:"default",error:a,state:{tags:{activeTags:[]}},serverRendered:true,routePath:"\u002Fgarden\u002Ftags\u002FLinguistics",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7f46b7a0"}}}}(null));
